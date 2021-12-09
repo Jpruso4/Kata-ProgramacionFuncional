2e4c403cac729c295a4b520a976a7418802a9bf5
Kevin Alcívar <60280778+kevealci@users.noreply.github.com>
2021-12-09 17:33:36 -0500

    Kata 6 resuelta

---
+++ src/main/java/katas/Kata6.java
@@ -0,0 +1,22 @@
+package katas;
+
+import model.Movie;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+    Goal: Retrieve the url of the largest boxart using map() and reduce()
+    DataSource: DataUtil.getMovieLists()
+    Output: String
+*/
+public class Kata6 {
+    public static String execute() {
+        return DataUtil.getMovieLists().stream()
+                .flatMap(value -> value.getVideos().stream().flatMap( video -> video.getBoxarts().stream()))
+                .reduce( (acum, actual) -> acum.getWidth() * acum.getHeight() > actual.getWidth() * actual.getHeight() ?  acum : actual )
+                .get().getUrl();
+
+    }
+}
